c932709595c75a1a1f46eca35b0d784a615e588a
“Anshika <ansgupta.01@gmail.com>
2023-02-27 23:49:22 +0530

    update booking code

--- backend/controllers/booking-controller.js
+++ backend/controllers/booking-controller.js
@@ -5,18 +5,18 @@ const newBooking = async (req, res, next) => {
     let booking;
     try {
        
-        newBooking = new Bookings({
+        booking = new Bookings({
             movie,
             date: new Date(`${date}`),
             seatNumber,
             user,
         });
-        newBooking = await newBooking.save();
+        booking = await booking.save();
     }
     catch (error) {
         console.log(error)
     }
-    if (!newBooking) {
+    if (!booking) {
         return res.status(400).json({ message: "Something went wrong to create booking" });
     }
 return res.status(201).json({Bookings: booking})
